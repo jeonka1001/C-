a01b253d29e099bde419e222ab83262e7b9132fb
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2020-01-07 21:04:04 +0900

    Add files via upload

---
+++ cstring 함수 연습.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  2-6 마지막 문제
+//
+//  Created by 전경안 on 2020/01/07.
+//  Copyright © 2020 전경안. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char apple[30];
+    cout<<"사과는 영어로?:";
+    cin>>apple;
+    cout<<"글자수: "<<strlen(apple)<<endl;
+    
+    char cpyapple[30];
+    strcat(apple," pencil");
+    strcpy(cpyapple,"apple pencil");
+    cout<<"내가 쓰는 펜슬은: "<<apple<<endl;
+
+    if(strcmp(apple,cpyapple)==0){
+        cout<<"같은 글자야!"<<endl;
+    }
+    else{
+        cout<<"다른 글자야!"<<endl;
+    }
+    return 0;
+}
