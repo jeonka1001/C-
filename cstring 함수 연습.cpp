b7624a8aa452abec60cd93a3e07935f6e40cbeb1
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2020-01-07 21:05:32 +0900

    Update cstring 함수 연습.cpp

--- cstring 함수 연습.cpp
+++ cstring 함수 연습.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  2-6 마지막 문제
+//  2-6 마지막 문제
 //
 //  Created by 전경안 on 2020/01/07.
 //  Copyright © 2020 전경안. All rights reserved.
@@ -15,14 +15,14 @@ int main() {
     char apple[30];
     cout<<"사과는 영어로?:";
     cin>>apple;
-    cout<<"글자수: "<<strlen(apple)<<endl;
+    cout<<"글자수: "<<strlen(apple)<<endl;//strlen 함수 확인
     
     char cpyapple[30];
     strcat(apple," pencil");
     strcpy(cpyapple,"apple pencil");
-    cout<<"내가 쓰는 펜슬은: "<<apple<<endl;
+    cout<<"내가 쓰는 펜슬은: "<<apple<<endl;//strcat 함수 확인
 
-    if(strcmp(apple,cpyapple)==0){
+    if(strcmp(apple,cpyapple)==0){//strcpy,strcmp함수 확인
         cout<<"같은 글자야!"<<endl;
     }
     else{
